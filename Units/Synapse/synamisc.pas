7eff850daea7805eedaa863e53feedd05cdabfea
William Carey <willtcarey@gmail.com>
2014-09-23 08:26:09 -0500

    Updating Synapse library to something that actually compiles on OS X

--- Units/Synapse/synamisc.pas
+++ Units/Synapse/synamisc.pas
@@ -1,5 +1,5 @@
 {==============================================================================|
-| Project : Ararat Synapse                                       | 001.003.000 |
+| Project : Ararat Synapse                                       | 001.003.001 |
 |==============================================================================|
 | Content: misc. procedures and functions                                      |
 |==============================================================================|
@@ -50,6 +50,15 @@
 {$Q-}
 {$H+}
 
+//Kylix does not known UNIX define
+{$IFDEF LINUX}
+  {$IFNDEF UNIX}
+    {$DEFINE UNIX}
+  {$ENDIF}
+{$ENDIF}
+
+{$TYPEDADDRESS OFF}
+
 {$IFDEF UNICODE}
   {$WARN IMPLICIT_STRING_CAST OFF}
   {$WARN IMPLICIT_STRING_CAST_LOSS OFF}
@@ -68,12 +77,15 @@ interface
 {$ENDIF}
 
 uses
-  synautil, blcksock, SysUtils, Classes,
-{$IFDEF LINUX}
-  Libc;
+  synautil, blcksock, SysUtils, Classes
+{$IFDEF UNIX}
+  {$IFNDEF FPC}
+  , Libc
+  {$ENDIF}
 {$ELSE}
-  Windows;
+  , Windows
 {$ENDIF}
+;
 
 Type
   {:@abstract(This record contains information about proxy setting.)}
@@ -145,7 +157,7 @@ end;
 
 {==============================================================================}
 
-{$IFNDEF LINUX}
+{$IFNDEF UNIX}
 function GetDNSbyIpHlp: string;
 type
   PTIP_ADDRESS_STRING = ^TIP_ADDRESS_STRING;
@@ -237,7 +249,7 @@ end ;
 {$ENDIF}
 
 function GetDNS: string;
-{$IFDEF LINUX}
+{$IFDEF UNIX}
 var
   l: TStringList;
   n: integer;
@@ -284,7 +296,7 @@ end;
 {==============================================================================}
 
 function GetIEProxy(protocol: string): TProxySetting;
-{$IFDEF LINUX}
+{$IFDEF UNIX}
 begin
   Result.Host := '';
   Result.Port := '';
